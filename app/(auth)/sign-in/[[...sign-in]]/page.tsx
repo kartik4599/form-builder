fd152c466c6aea94b2366a8b38fc0ea718b9805f
kartik <kartikmendu@gmail.com>
2024-05-06 19:15:31 +0530

    Added Sign-In and Sign-up page.

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <SignIn path="/sign-in" />
+    </div>
+  );
+}
