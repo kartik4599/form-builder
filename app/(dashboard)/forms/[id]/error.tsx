b94b08e3d8806b6c10617447e83e86512108ca1a
kartik4599 <kartikmendu@gmail.com>
2024-05-12 19:15:59 +0530

    Added icon and changes in error pages.

--- app/(dashboard)/forms/[id]/error.tsx
+++ app/(dashboard)/forms/[id]/error.tsx
@@ -2,15 +2,10 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useEffect } from "react";
 
-const error = ({ error }: { error: Error }) => {
-  useEffect(() => {
-    console.log(error);
-  }, [error]);
-
+const error = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-4">
+    <div className="flex w-full flex-col items-center justify-center gap-4 my-auto">
       <h2 className="text-destructive text-4xl">Something went wrong!</h2>
       <Button asChild>
         <Link href={"/dashboard"}>Go back home</Link>
